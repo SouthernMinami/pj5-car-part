fde27c392b265fee54bef8489b9a04fe672b6907
SouthernMinami <kanou373@gmail.com>
2024-03-07 05:31:10 +0900

    Add: DBにアクセス

---
+++ Database/MySQLWrapper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database;
+
+use mysqli;
+use Helpers\Settings;
+
+class MySQLWrapper extends mysqli {
+    public function __construct(?string $hostname = 'localhost', ?string $username = null, ?string $password = null, ?string $database = null, ?int $port = null, ?string $socket = null) {
+        // 接続失敗時には例外を投げるように設定
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $username = $username ?? Settings::env('DATABASE_USER');
+        $password = $password ?? Settings::env('DATABASE_USER_PASSWORD');
+        $database = $database ?? Settings::env('DATABASE_NAME');
+
+        parent::__construct($hostname, $username, $password, $database, $port, $socket);
+    }
+    
+    //　デフォルトのデータベース名を取得
+    public function getDatabaseName(): string {
+        return $this->query("SELECT database() AS the_db")->fetch_row()[0];
+    }
+}
--- init-app.php
+++ init-app.php
@@ -1,4 +1,5 @@
 <?php
+
 spl_autoload_extensions(".php");
 spl_autoload_register(function ($class) {
     // クラス名から名前空間を取得
@@ -12,5 +13,19 @@ spl_autoload_register(function ($class) {
 
 use Helpers\Settings;
 
-printf("Local database username: %s\n", Settings::env('DATABASE_USER'));
-printf("Local database password (hashed): %s\n", password_hash(Settings::env('DATABASE_USER_PASSWORD'), PASSWORD_DEFAULT));
+// 接続失敗時には例外を投げるように設定
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$mysqli = new mysqli('localhost', Settings::env('DATABASE_USER'), Settings::env('DATABASE_USER_PASSWORD'), Settings::env('DATABASE_NAME'));
+
+$charset = $mysqli->get_charset();
+
+if ($charset === null) {
+    throw new Exception('Charset could be read from the database');
+}
+
+// DBの文字セット、照合順序、統計情報を取得
+printf("%s'%s charset", Settings::env('DATABASE_NAME'), $charset->charset, PHP_EOL);
+printf("collation: %s'%s ", $charset->collation, PHP_EOL);
+
+$mysqli->close();
